1f18b82e62ef8ff04aa80a0a4e16b6a149358505
Jonathan Boswell <jonathan@arborbridge.com>
2017-04-01 16:52:29 -0700

    99 homework

---
+++ 08-intro-to-dom-and-jquery/5_homework/99.js
@@ -0,0 +1,6 @@
+// Reuse the 99 Bottles of Beer assignment to create a website that programatically displays the lyrics to that song.
+
+$(function(){
+    //Write a for loop
+        //have that for loop inject the lyrics into a list item in an HTML file
+});
